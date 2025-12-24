e9ac1859e8c49ffcafa9e0279ffc1ea4e08c9840
cagri.yolyapar <cagri.yolyapar@gmail.com>
2025-12-24 10:11:18 +0300

    FabricateCompletion tamamlandi

--- src/app/myComponents/fabricate-completion/fabricate-completion.ts
+++ src/app/myComponents/fabricate-completion/fabricate-completion.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FabricateService } from '../../Services/FabricateService';
+import { RouterModule,Router } from '@angular/router';
+import { AuthService } from '../../Services/AuthService';
 
 @Component({
   selector: 'app-fabricate-completion',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './fabricate-completion.html',
   styleUrl: './fabricate-completion.css',
 })
 export class FabricateCompletion {
+  fabricateService = inject(FabricateService);
+  private router = inject(Router);
 
+  private authService = inject(AuthService);
+
+  logout(){
+    this.authService.logout();
+
+    this.router.navigateByUrl('/ships');
+  }
 }
--- src/app/myComponents/login-system/login-system.ts
+++ src/app/myComponents/login-system/login-system.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../Services/AuthService';
 
 @Component({
   selector: 'app-login-system',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login-system.html',
   styleUrl: './login-system.css',
 })
 export class LoginSystem {
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  onLogin():void{
+    this.authService.login();
+    this.router.navigate(['/ships']);
+  }
 }
